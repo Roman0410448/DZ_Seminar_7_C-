36ce2fdb7efc9354a7f4f90906bd9dd93585acc2
ROMAN <junk0410448@mail.ru>
2023-04-03 21:34:54 +0700

    z1

---
+++ Z_1/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+// m - строчки, n - столбцы
+double[,] array2D = GetMatrix(rows,columns);
+double[,] GetMatrix(int m, int n)
+{ // [rows, columns]
+double[,] arr = new double[m, n];
+   Random rand = new Random();
+for (int i = 0; i<m; i++) // matrix.GetLength(0) = m - строчки
+{
+// i, j , m , k
+for (int j = 0; j < n; j++) // matrix.GetLength(1) = n - столбцы
+{
+    arr[i,j] =rand.NextDouble()*100;
+}
+}
+return arr;
+}
+
+PrintMatrix(array2D);  // inputMatrix = array2D
+// Console.WriteLine("Вывод матрицы  2 на 2 для теста: ");
+// PrintMatrix(testMatrix); // inputMatrix = testMatrix
+// Console.WriteLine("Вывод матрицы  3 на 3 для теста: ");
+// PrintMatrix(secondTestMatrix); // inputMatrix = secondTestMatrix
+// Печатаем двумерный массив на экран
+void PrintMatrix(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < inputMatrix.GetLength(1); j++) // столбцы
+        {
+            Console.Write(inputMatrix[i, j] + "\t"); // "\t" - отступ между элементами   
+        }
+        Console.WriteLine(); // Переход на новую строчку
+    }
+}
